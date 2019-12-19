facba4e036a07f250eb5dcfa0e08f05a8ed4faca
parag-vinsol <parag@vinsol.com>
2019-12-19 18:56:47 +0530

    css changes

--- src/components/DisplayBlock/SingleTweetBlock.js
+++ src/components/DisplayBlock/SingleTweetBlock.js
@@ -8,11 +8,12 @@ class SingleTweetBlock extends Component {
     return(
       <Fragment>
         <div className="SingleTweetBlock">
-          {this.props.tweetsToBeDisplayed} 
-          <br></br>
-          <button className="btn" onClick={() => this.props.onDeletingTweet(this.props.id)}><i  className="fa fa-close"></i></button>
-          <button className="btnEdit" onClick={() => this.props.openEditModal(this.props.id)}><i className="fa fa-edit"></i></button>
-          {this.props.isEdited ? <p className="Edited">edited</p> : ''}
+          <p>{this.props.tweetsToBeDisplayed}</p>
+          <div className="optionsContainer">
+            {this.props.isEdited ? <p className="Edited">edited</p> : ''}
+            <button className="btn" onClick={() => this.props.onDeletingTweet(this.props.id)}><i  className="fa fa-close"></i></button>
+            <button className="btnEdit" onClick={() => this.props.openEditModal(this.props.id)}><i className="fa fa-edit"></i></button>
+          </div>
         </div>
       </Fragment>
         
